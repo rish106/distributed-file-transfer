f03935d8f54af96f58b9fb8c879f8f641f382656
Rishabh Verma <10rishabhv@gmail.com>
2023-09-08 23:08:06 +0530

    final submission

--- client.py
+++ client.py
@@ -1,74 +1,69 @@
 import socket
-import time
+from time import perf_counter
 import threading
-from queue import Queue
 
 lines = {}
 
 MAX_LINES = 1000
 
-# clients_info = ["192.168.105.50:10000", "192.168.105.79:10000"]
-clients_info = []
-OTHER_CLIENTS = len(clients_info)
+OTHER_CLIENTS = 3
+lines_received = []
 
 connected_clients = 0
 
 ENTRY_NUMBER = "2021CS10581"
 TEAM_NAME = "slowbrains"
+
 server_conn = socket.socket()
+
+# sockets from which unique lines are sent to other clients
 sending_client_sockets = []
+
+# sockets from which unique lines are received from other clients
 receiving_client_sockets = []
+
+# threads receiving and processing the lines from other clients
 receiving_threads = []
-print("Created receiving sockets at ports:")
-for i in range(OTHER_CLIENTS):
-    client_socket = socket.socket()
-    host = ""
-    port = 10000 + i
-    print(port, end = "")
-    if i == OTHER_CLIENTS - 1:
-        print()
-    else:
-        print(" ", end = "")
-    client_socket.bind((host, port))
-    client_socket.listen(5)
-    receiving_client_sockets.append(client_socket)
-sending_data = Queue()
+
+stop_event = threading.Event()
 
 
 def worker_function(i):
-    conn, address = receiving_client_sockets[i].accept()
-    print(f"Connection established with IP {address[0]}")
+    try:
+        conn, address = receiving_client_sockets[i].accept()
+    except:
+        return
     line_number = 0
     content = ""
     reading_content = False
-    while True:
+    while not stop_event.is_set():
         data = conn.recv(4096)
         response = data.decode("utf-8")
-        for i in range(len(response)):
-            if response[i] == '\n':
+        for j in range(len(response)):
+            if response[j] == '\n':
                 if reading_content:
+                    global lines_received
                     lines[line_number] = content
+                    lines_received[i] += 1
                     line_number = 0
                     content = ""
                 reading_content = not reading_content
             elif not reading_content:
-                line_number = 10 * line_number + (response[i] - '0')
+                line_number = 10 * line_number + (ord(response[j]) - ord('0'))
             else:
-                content += response[i]
-
-
-def sending_function():
-    while True:
-        if not sending_data.empty():
-            data = sending_data.get()
-            for client in sending_client_sockets:
-                client.send(str.encode(data))
+                content += response[j]
 
 
 def receive_response():
     data = server_conn.recv(4096)
     response = data.decode("utf-8")
-    return response
+    if len(response) == 0:
+        try:
+            return receive_response()
+        except:
+            return ""
+    else:
+        return response
 
 
 def connect_to_server(cmd_arg):
@@ -77,7 +72,7 @@ def connect_to_server(cmd_arg):
         return
     tokens = cmd_arg.split(":")
     if len(tokens) != 2:
-        print("Invalid format, type 'CONNECTSERVER {IP_ADDRESS}:{HOST}'")
+        print("Invalid format, type 'SERVER {IP_ADDRESS}:{HOST}'")
         return
     ip_address = tokens[0]
     port = int(tokens[1])
@@ -94,14 +89,12 @@ def connect_to_client(cmd_arg):
         return
     tokens = cmd_arg.split(":")
     if len(tokens) != 2:
-        print("Invalid format, type 'CONNECTCLIENT {IP_ADDRESS}:{HOST}'")
+        print("Invalid format, type 'CLIENT {IP_ADDRESS}:{HOST}'")
         return
     ip_address = tokens[0]
     port = int(tokens[1])
     try:
         global connected_clients
-        receiving_threads.append(threading.Thread(target = worker_function, daemon = True, args = (connected_clients,)))
-        receiving_threads[connected_clients].start()
         sending_client_sockets.append(socket.socket())
         sending_client_sockets[connected_clients].connect((ip_address, port))
         connected_clients += 1
@@ -111,27 +104,32 @@ def connect_to_client(cmd_arg):
 
 
 def infinite_requests():
-    actual_start = time.time()
-    start = time.time()
-    total_time = 0
-    total_requests = 0
+    start = perf_counter()
+    successful_requests = 0
     line_number = 0
     content = ""
     reading_content = False
     completed_line = False
     def receive_line():
-        nonlocal content, line_number, reading_content, total_requests, completed_line
-        response = receive_response()
-        if len(response) == 0:
-            receive_line()
+        nonlocal content, line_number, reading_content, successful_requests, completed_line
+        data = server_conn.recv(4096)
+        response = data.decode("utf-8")
         for i in range(len(response)):
             if response[i] == '\n':
                 if reading_content:
                     if line_number not in lines and line_number >= 0 and line_number < MAX_LINES:
                         lines[line_number] = content
+                        # sending_data.put(str(line_number) + "\n" + content + "\n")
+                        for client in sending_client_sockets:
+                            try:
+                                client.send(str.encode(f"{line_number}\n{content}\n"))
+                            except:
+                                pass
                     line_number = 0
                     content = ""
                     completed_line = True
+                    if line_number >= 0 and line_number < MAX_LINES:
+                        successful_requests += 1
                 reading_content = not reading_content
             elif not reading_content:
                 line_number = 10 * line_number + (ord(response[i]) - ord('0'))
@@ -139,22 +137,23 @@ def infinite_requests():
                 content += response[i]
     while len(lines) < MAX_LINES:
         server_conn.send(str.encode("SENDLINE\n"))
-        total_requests += 1
-        curr = time.time()
-        if (curr - start >= 5):
-            start = curr
-            total_time += 5
-            print(len(lines), total_time)
         completed_line = False
         while not completed_line:
             receive_line()
     else:
-        print(f"Completed in {time.time() - actual_start} seconds with {total_requests} requests from this client")
+        print(f"Completed in {perf_counter() - start} seconds with {successful_requests} successful requests from this client")
         submit_lines()
+        if len(lines_received) > 0:
+            print("Received lines from other clients: ", end = "")
+        for i in lines_received:
+            print(i, end = " ")
+        print()
+        for i in range(len(lines_received)):
+            lines_received[i] = 0
 
 
 def submit_lines():
-    submit_data = "SUBMIT\n" + ENTRY_NUMBER + "@" + TEAM_NAME + "\n"
+    submit_data = f"SUBMIT\n{ENTRY_NUMBER}@{TEAM_NAME}\n"
     submit_data += str(len(lines)) + "\n"
     line_list = []
     for i in lines:
@@ -179,6 +178,23 @@ def get_incorrect_lines():
     print(response, end = "")
 
 
+def get_argument(cmd):
+    tokens = cmd.split(' ')
+    if len(tokens) < 2:
+        return ""
+    else:
+        return tokens[1]
+
+
+def set_entry_number(new_entry_number):
+    global ENTRY_NUMBER
+    if len(new_entry_number) == 0:
+        print("Invalid entry number")
+        return
+    ENTRY_NUMBER = new_entry_number
+    print(f"Successfully set entry number to {ENTRY_NUMBER}")
+
+
 def handle_command(cmd):
     if cmd == "":
         print("Please enter a command")
@@ -188,29 +204,21 @@ def handle_command(cmd):
     elif cmd == "START":
         infinite_requests()
     elif cmd.startswith("SERVER"):
-        tokens = cmd.split(" ")
-        if len(tokens) < 2:
-            address_port = ""
-        else:
-            address_port = tokens[1]
+        address_port = get_argument(cmd)
         connect_to_server(address_port)
     elif cmd.startswith("CLIENT"):
-        tokens = cmd.split(" ")
-        if len(tokens) < 2:
-            address_port = ""
-        else:
-            address_port = tokens[1]
+        address_port = get_argument(cmd)
         connect_to_client(address_port)
+    elif cmd.startswith("ENTRYNUMBER"):
+        entry_number = get_argument(cmd)
+        set_entry_number(entry_number)
     elif cmd == "SUBMIT":
         submit_lines()
     elif cmd == "CLEAR":
         lines.clear()
         print("Cleared dictionary")
     elif cmd == "PRINT":
-        print("Unique lines =", len(lines))
-    elif cmd == "CO":
-        for client in clients_info:
-            connect_to_client(client)
+        print(f"Unique lines = {len(lines)}")
     elif cmd == "INCORRECT":
         get_incorrect_lines()
     elif cmd == "RESET":
@@ -220,8 +228,27 @@ def handle_command(cmd):
     return True
 
 
+def init():
+    if OTHER_CLIENTS > 0:
+        print("Created receiving sockets at ports:")
+    for i in range(OTHER_CLIENTS):
+        client_socket = socket.socket()
+        lines_received.append(0)
+        host = ""
+        port = 10000 + i
+        print(port, end = "")
+        if i == OTHER_CLIENTS - 1:
+            print()
+        else:
+            print(" ", end = "")
+        client_socket.bind((host, port))
+        client_socket.listen(5)
+        receiving_client_sockets.append(client_socket)
+        receiving_threads.append(threading.Thread(target = worker_function, daemon = True, args = (i,)))
+        receiving_threads[i].start()
+
+
 def start_command():
-    connect_to_server("10.17.6.5:9801")
     try:
         while True:
             cmd = input()
@@ -231,6 +258,7 @@ def start_command():
         print("\nConnection closed by user")
     finally:
         server_conn.close()
+        stop_event.set()
         for sock in sending_client_sockets:
             sock.close()
         for sock in receiving_client_sockets:
@@ -238,8 +266,5 @@ def start_command():
         print("Closed all sockets")
 
 
-# sending_thread = threading.Thread(target = sending_function, daemon = True)
-# sending_thread.start()
-
-
+init()
 start_command()
